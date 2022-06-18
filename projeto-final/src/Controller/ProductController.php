27a031df9817576b661b3d3f7090ed07d2e77179
Danilo Nogueira <nvdanilo@gmail.com>
2022-06-18 00:23:09 -0300

    Ajustes do PDF

--- projeto-final/src/Controller/ProductController.php
+++ projeto-final/src/Controller/ProductController.php
@@ -94,7 +94,43 @@ class ProductController extends AbstractController
 
     public function reportAction(): void
     {
-        $html = "<h1>Relatório</h1>";
+        $con = Connection::getConnection();
+        $result = $con->prepare("SELECT prod.id, prod.name, prod.quantity, cat.name AS category FROM tb_product prod INNER JOIN tb_category cat ON prod.category_id = cat.id");
+        $result->execute();
+
+        $content = "";
+
+        while ($product = $result->fetch(\PDO::FETCH_ASSOC)) {
+            
+            extract($product);
+            
+            $content .= "
+                <tr>
+                    <td>{$id}</td>
+                    <td>{$name}</td>
+                    <td>{$quantity}</td>
+                    <td>{$category}</td>
+                </tr>
+            ";
+        }
+
+
+        $html = "
+            <h1>Relatório de Produtos no Estoque</h1>
+            <table border='1' width='100%'>
+                <thead>
+                    <tr>
+                        <th>#ID</th>
+                        <th>Nome</th>
+                        <th>Estoque</th>
+                        <th>Categoria</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {$content}
+                </tbody>
+            </table>
+        ";
 
         $pdf = new Dompdf();
         $pdf->loadHtml($html);
